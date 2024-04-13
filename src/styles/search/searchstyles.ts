6e4e723cf7a7091311d761d01d56743c49901fcf
Nishant Banjade <nishantbanjade07@gmail.com>
2024-04-13 08:45:59 +0545

    add: searchStyles wrapper for search component

---
+++ src/styles/search/searchstyles.ts
@@ -0,0 +1,33 @@
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import { Colors } from "../basetheme/basetheme";
+import { Button } from "@mui/material";
+export const SearchBarContainer = styled(Box)(({ theme }) => ({
+  width: "auto",
+  height: "10vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "white",
+  marginRight: "35em",
+}));
+
+export const SearchBar = styled(Box)(({ theme }) => ({
+  margin: "0em",
+}));
+
+export const SearchButton = styled(Box)(() => ({
+  ...SearchBar,
+  color: Colors.biscay,
+  background: Colors.pampas,
+}));
+
+export const StyledButton = styled(Button)`
+  && {
+    margin-left: 0.7em;
+    padding: 15px;
+    width: 10em;
+    color: ${Colors.pampas};
+    background-color: ${Colors.title};
+  }
+`;
